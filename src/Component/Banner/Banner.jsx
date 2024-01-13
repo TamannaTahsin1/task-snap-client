18ac68bdf23c3988754f7b3d569f1ec0d306115b
TamannaTahsin1 <tamanntahsin648@gmail.com>
2024-01-14 00:51:44 +0600

    banner section has been created

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
             <p className="text-xl font-semibold">Amit Patel</p>
             <div className="bg-[#978CD0] mt-5 p-8 rounded-xl flex justify-between items-center">
             <h1 className="text-sm text-white font-normal">Attendance</h1>
-            <div className="radial-progress text-white w-28 h-28" style={{"--value":93}} role="progressbar">93%</div>
+            <div className="radial-progress text-white w-24 h-24" style={{"--value":93}} role="progressbar">93%</div>
             </div>
         </div>
     );
